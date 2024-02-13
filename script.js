56f92d456fc04071ada06dd29a955693b9f29f0c
Qarshiboyev Bobur <you@example.com>
2024-02-13 14:05:40 +0500

    jefwrnhenhue

---
+++ script.js
@@ -0,0 +1,24 @@
+
+let a = document.querySelector('.contact')
+let b = document.querySelector('.img1')
+let d = document.querySelector('.img2')
+let c = document.querySelector('.img3')
+let e = document.querySelector('.img4')
+let f = document.querySelector('.img5')
+
+
+document.querySelector('b').onclick = () => {
+    a.document.querySelector('b')
+}
+document.querySelector('.img2').onclick = () => {
+    document.querySelector('.contact').innerHTML.add('.img2')
+}
+document.querySelector('.img3').onclick = () => {
+    document.querySelector('.contact').innerHTML.add('.img3')
+}
+document.querySelector('.img4').onclick = () => {
+    document.querySelector('.contact').innerHTML.add('.img4')
+}
+document.querySelector('.img5').onclick = () => {
+    document.querySelector('.contact').innerHTML.add('.img5')
+}
